62a0620921253f910f2d1f964a475428b0956eb6
EarthenPotato <61642489+EarthenPotato@users.noreply.github.com>
2023-10-03 17:48:12 +0800

    soem progress on cs3 menu

--- casestudy3/nochange.js
+++ casestudy3/nochange.js
@@ -5,13 +5,61 @@
 // The event handler function to compute the cost
 
 function computeCost() {
-  var french = document.getElementById("french").value;
-  var hazlenut = document.getElementById("hazlenut").value;
-  var columbian = document.getElementById("columbian").value;
-
+  var Just_Java = document.getElementById("1").value;
+  var Cafe_au_Lait_s = document.getElementById("2").value;
+  var Cafe_au_Lait_d = document.getElementById("3").value;
+  var Iced_Cappuccino_s = document.getElementById("4").value;
+  var Iced_Cappuccino_d = document.getElementById("5").value;
 // Compute the cost
 
   document.getElementById("cost").value = 
-  totalCost = french * 3.49 + hazlenut * 3.95 + 
-              columbian * 4.59;
+  totalCost = Just_Java * 2.00
+            + Cafe_au_Lait_s * 2.00
+            + Cafe_au_Lait_d * 3.00
+            + Iced_Cappuccino_s * 4.75
+            + Iced_Cappuccino_d * 5.75;
 }  //* end of computeCost
+
+function computeSubCost(Cafe_au_Lait_price,Iced_Cappuccino_price,Just_Java_price){
+  var Just_Java = document.getElementById("Java_num").value
+  var Cafe_au_Lait = document.getElementById("Cafe_num").value
+  var Iced_Cappuccino = document.getElementById("Iced_num").value
+
+  Java_cost = Just_Java * Just_Java_price
+  Cafe_au_Lait_cost = Cafe_au_Lait * Cafe_au_Lait_price
+  Iced_Cappuccino_cost = Iced_Cappuccino * Iced_Cappuccino_price
+
+  sub_cost = Java_cost + Cafe_au_Lait_cost + Iced_Cappuccino_cost
+}
+function radio_value(drink){
+  var dom = document.getElementById("myForm")
+  
+  for (var index = 0; index < dom.planeButton.length; 
+      index++) {
+    if (dom.planeButton[index].checked) {
+      drink = dom.planeButton[index].value;
+      break;
+      }}
+}
+
+switch (drink) {
+  case "1": 
+    price = computeSubCost(0,0,2);
+    console.log(price)
+    break;
+  case "2": 
+    computeSubCost(0,2,0);
+    break; 
+  case "3":
+    computeSubCost(0,3,0);
+    break;    
+  case "4":
+    computeSubCost(4.75,0,0);
+    break; 
+  case "5":
+    computeSubCost(5.75,0,0);
+    break;
+  default:
+    alert("Please select a drink");
+    break;
+}
