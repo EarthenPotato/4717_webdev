09cdfadc73148928c3b14e14803df298efac580a
EarthenPotato <61642489+EarthenPotato@users.noreply.github.com>
2023-10-04 15:43:48 +0800

    completed backend of subcost function

--- casestudy3/nochange.js
+++ casestudy3/nochange.js
@@ -3,7 +3,9 @@
 //   to prevent the user from changing a text field
 
 // The event handler function to compute the cost
-
+var Java_btn
+var Cafe_btn
+var Iced_btn
 function computeCost() {
   var Just_Java = document.getElementById("1").value;
   var Cafe_au_Lait_s = document.getElementById("2").value;
@@ -20,7 +22,7 @@ function computeCost() {
             + Iced_Cappuccino_d * 5.75;
 }  //* end of computeCost
 
-function computeSubCost(Cafe_au_Lait_price,Iced_Cappuccino_price,Just_Java_price){
+function computeSubCost(Iced_Cappuccino_price,Cafe_au_Lait_price,Just_Java_price){
   var Just_Java = document.getElementById("Java_num").value
   var Cafe_au_Lait = document.getElementById("Cafe_num").value
   var Iced_Cappuccino = document.getElementById("Iced_num").value
@@ -30,17 +32,15 @@ function computeSubCost(Cafe_au_Lait_price,Iced_Cappuccino_price,Just_Java_price
   Iced_Cappuccino_cost = Iced_Cappuccino * Iced_Cappuccino_price
 
   sub_cost = Java_cost + Cafe_au_Lait_cost + Iced_Cappuccino_cost
-  
+  console.log('this is subcost', sub_cost)
   return sub_cost
 }
 
 function radio_value(drink_name){
   console.log(drink_name);
   var dom = document.getElementsByName(drink_name)
-  // console.log(dom)
   
-  for (var index = 0; index < dom.length; 
-      index++) {
+  for (var index = 0; index < dom.length; index++) {
     console.log(index);
     if (dom[index].checked) {
       drink = dom[index].value;
@@ -50,25 +50,61 @@ function radio_value(drink_name){
 
   switch (drink) {
     case '1': 
-      price = computeSubCost(0,0,2);
-      console.log(price);
+      computeSubCost(0,0,2);
+      Java_btn = 0
       break;
     case '2': 
       computeSubCost(0,2,0);
+      Cafe_btn = 0
       break; 
     case '3':
       computeSubCost(0,3,0);
-      console.log(price);
+      Cafe_btn = 1
       break;    
     case '4':
       computeSubCost(4.75,0,0);
+      Iced_btn = 0
       break; 
     case '5':
       computeSubCost(5.75,0,0);
+      Iced_btn = 1
       break;
     default:
       alert("Please select a drink");
       break;
   }
+  return drink 
 }
 
+function update_sub_price(drink_name){
+  if (drink_name == 'Java'){
+    if (Java_btn == 0){
+      computeSubCost(0,0,2)
+    }
+    else{
+      alert("Please select a drink");
+    }
+  }
+  if (drink_name == 'Cafe'){
+    if (Cafe_btn == 0){
+      computeSubCost(0,2,0);
+    }
+    else if(Cafe_btn == 1){
+      computeSubCost(0,3,0);
+    }
+    else{
+      alert("Please select a drink");
+    }
+  }
+  if (drink_name == 'Iced'){
+    if (Iced_btn == 0){
+      computeSubCost(4.75,0,0);
+    }
+    else if (Iced_btn == 1){
+      computeSubCost(5.75,0,0);
+    }
+    else{
+      alert("Please select a drink");
+    }
+  }
+}
