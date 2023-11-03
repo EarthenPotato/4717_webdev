2a45523137d88e10b67e3a956b4d659822b9eda2
x9no <april.qb.jiang@gmail.com>
2023-11-03 17:24:06 +0800

    product description

--- Final_Project/gen_DB.sql
+++ Final_Project/gen_DB.sql
@@ -6,11 +6,40 @@ CREATE TABLE customers (
     phone_number VARCHAR(15),
     orderID INT
 );
+
 CREATE TABLE order_list (
-    customername VARCHAR(255),
-    quantity INT,
-    price DECIMAL(10, 2)
+    ID INT PRIMARY KEY,
+    customername VARCHAR(255)
 );
+
+CREATE TABLE order_list_quantity (
+    ID INT PRIMARY KEY,
+    AQ1 INT,
+    AQ2 INT,
+    AQ3 INT,
+    CQ1 INT,
+    CQ2 INT,
+    CQ3 INT,
+    TQ1 INT,
+    TQ2 INT,
+    TQ3 INT
+);
+
+CREATE TABLE order_list_price (
+    ID INT PRIMARY KEY,
+    AP1 DECIMAL(10,2),
+    AP2 DECIMAL(10,2),
+    AP3 DECIMAL(10,2),
+    CP1 DECIMAL(10,2),
+    CP2 DECIMAL(10,2),
+    CP3 DECIMAL(10,2),
+    TP1 DECIMAL(10,2),
+    TP2 DECIMAL(10,2),
+    TP3 DECIMAL(10,2)
+);
+
+
 CREATE TABLE product (
+    item VARCHAR(255),
     price DECIMAL(10, 2)
 );
